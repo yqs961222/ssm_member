f03f0074b83c395b3cc32837f2aecf9fa2399deb
xiongtianle <2529814288@qq.com>
2020-07-13 12:05:09 +0800

    新增service层接口及实现类，BrandManageController注入service，连接数据库

--- src/main/java/com/qfedu/controller/BrandManageController.java
+++ src/main/java/com/qfedu/controller/BrandManageController.java
@@ -1,5 +1,16 @@
 package com.qfedu.controller;
 
+import com.qfedu.service.BrandManageService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/brand_manage")
 public class BrandManageController {
+    @Resource
+    BrandManageService brandManageService;
+
 
 }
