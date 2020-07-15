e0533daab8d7faefa207c3d89bc2b3536f456b72
Jesse Yuan <1831232665@qq.com>
2020-07-15 08:45:45 +0800

    新增品牌详细的按照属性查询的接口

--- src/main/java/com/qfedu/mapper/BrandDetailMapper.java
+++ src/main/java/com/qfedu/mapper/BrandDetailMapper.java
@@ -1,56 +1,26 @@
 package com.qfedu.mapper;
 
 import com.qfedu.domain.entity.BrandDetail;
-import org.apache.ibatis.annotations.Mapper;
+import com.qfedu.exception.DaoException;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
-@Mapper
 public interface BrandDetailMapper {
 
     /**
-     * 显示该品牌所有商品
-     * @param id 品牌id
-     * @param page 分页页数
-     * @param size 分页显示数量
-     * @return 返回查询所有品牌的结果集
+     * 通过属性查询商品信息
      */
-    List<BrandDetail> selectAllById(@Param("id") int id, @Param("page") int page, @Param("size") int size);
+    List<BrandDetail> selectByAttr(@Param("attr") String attr) throws DaoException;
 
     /**
-     * 添加品牌
-     * @param brandDetail
-     * @return 返回添加品牌影响的行数
+     * 工具方法, 获取属性的个数
      */
-    int insertProduct(@Param("brandDetail") BrandDetail brandDetail);
+    int selectNum() throws DaoException;
 
     /**
-     * 批量删除
-     * @param pid 批量删除的商品id的集合
-     * @return 返回影响的行数
+     * 工具方法, 获取属性的名称集合
      */
-    int deleteBath(@Param("pid") List<Integer> pid);
+    List<BrandDetail> selectGetAttr();
 
-    /**
-     * 删除一个商品
-     * @param proId 商品id
-     * @return 影响的行数
-     */
-    int deleteById(@Param("proId") int proId);
-
-    /**
-     * 修改商品信息
-     * @param brandDetail 商品信息
-     * @return 影响的行数
-     */
-    int updateBrand(@Param("brandDetail") BrandDetail brandDetail);
-
-    /**
-     * 封禁商品
-     * @param proId 商品id
-     * @param status 商品状态
-     * @return 影响的行数
-     */
-    int updateBannedById(@Param("pid") int pid, @Param("status") int status);
 }
--- src/main/java/com/qfedu/service/BrandDetailService.java
+++ src/main/java/com/qfedu/service/BrandDetailService.java
@@ -1,4 +1,33 @@
 package com.qfedu.service;
 
+import com.qfedu.domain.entity.BrandDetail;
+import com.qfedu.exception.ServiceException;
+
+import java.util.List;
+
+/**
+ * @Author: Jesse Y
+ * @Data: 2020-07-14 14:47
+ */
 public interface BrandDetailService {
+
+    /**
+     * 通过指定的属性attr查询商品信息
+     *
+     * @param attr 指定的属性
+     * @return 返回查找到的商品信息集合
+     */
+    List<BrandDetail> selectByAttr(String attr) throws ServiceException;
+
+    /**
+     * 工具方法, 获取属性的个数
+     * @return 返回个数
+     */
+    int selectNum() throws ServiceException;
+
+    /**
+     * 工具方法, 获取属性名称的集合
+     * @return 返回集合
+     */
+    List<BrandDetail> selectGetAttr() throws ServiceException;
 }
